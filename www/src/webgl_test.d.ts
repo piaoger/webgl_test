456e760be9aea8076ce4a68de713ee7ad770610a
piaoger <piaoger@yahoo.com>
2018-09-25 18:07:02 +0800

    remove typed file

--- www/src/webgl_test.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-export function init_bg(): number;
-
-export function load_map_bg(arg0: Uint8Array): number;
-
-export function main_loop_bg(arg0: number, arg1: any): number;
-
