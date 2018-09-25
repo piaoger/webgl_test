f163c1bdff808b7ddd554cae7f3c07ab5697011f
piaoger <piaoger@yahoo.com>
2018-09-25 18:06:13 +0800

    web project restructure

--- bin/build.sh
+++ bin/build.sh
@@ -12,10 +12,15 @@ set -ex
 
 PROJECT_NAME='webgl_test'
 
+
+mkdir -p ${APP_ROOT}/www/lib
+
+
+
 cargo +nightly build --target wasm32-unknown-unknown --release
 wasm-bindgen                                                     \
     ./target/wasm32-unknown-unknown/release/"$PROJECT_NAME".wasm \
-    --out-dir ${APP_ROOT}/www
+    --out-dir ${APP_ROOT}/www/lib
 
 cd ${APP_ROOT}/www
 
@@ -28,18 +33,19 @@ else
     # npm install tslint
 fi
 
+
 export PATH=$PATH:./node_modules/.bin/
 
 tslint -p tsconfig.json
-tsc
-wasm-gc ./"$PROJECT_NAME"_bg.wasm
+tsc --outDir ${APP_ROOT}/www/lib
+wasm-gc ${APP_ROOT}/www/lib/"$PROJECT_NAME"_bg.wasm
 
 #disable wasm-opt temporary
 #wasm-opt -O4 ./"$PROJECT_NAME"_bg.wasm
 webpack
-cp index.html ${APP_ROOT}/www/dist
-cp style.css ${APP_ROOT}/www/dist
-cp -a img ${APP_ROOT}/www/dist
+cp ${APP_ROOT}/www/static/index.html ${APP_ROOT}/www/dist
+cp ${APP_ROOT}/www/static/style.css ${APP_ROOT}/www/dist
+cp -a ${APP_ROOT}/www/static/img ${APP_ROOT}/www/dist
 cd ${APP_ROOT}
 
 
---
+++ www/src/webgl_test.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+export function init_bg(): number;
+
+export function load_map_bg(arg0: Uint8Array): number;
+
+export function main_loop_bg(arg0: number, arg1: any): number;
+
